18ac43c6195353c88d2befa7e54193ca5b485de1
JuliaGrisales <jgrisales@gmail.com>
2020-09-03 11:50:10 +0200

    Using Bootstrap in Laravel via CDN

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'IndexController@index');
+Route::get('/index', 'IndexController@index');
+Route::get('/create', 'IndexController@create');
+Route::get('/show', 'IndexController@show');
+Route::get('/articles', 'IndexController@store');
